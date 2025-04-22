10878fe17298ada9f3d6ad996c2be310c9bf9e77
Mark Wileman <watchandshoot@mail.co.uk>
2025-04-22 23:22:04 +0100

    Updated deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9cd61485ac45e5032e10a9e6fbd641d5",
+const RESOURCES = {"flutter_bootstrap.js": "2ea8f63b63edfb44c848c60f25bbdb46",
 "version.json": "ccfae18731012193fdd34aa0b7b62359",
 "index.html": "60c441518902fbea718558eb7687a7bf",
 "/": "60c441518902fbea718558eb7687a7bf",
-"main.dart.js": "3b91fa330d88e6336146366945c8254e",
+"main.dart.js": "4c17eb835c12aec658d6f1770129629c",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
